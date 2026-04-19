4436139723c155d2004e8bec06f7686320a0789a
Erik Schuessler <90277878+Brainspeck@users.noreply.github.com>
2026-04-19 02:35:27 +0000

    add function to get random elements

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 
+const suits = ["♦", "♥", "♠", "♣"];
+const values = ["A", "2", "3", "4", "5", "6","7", "8", "9", "10", "J", "Q", "K"];
+
+const getRandomElement = (array) => {
+    let randomElement = array[Math.floor(Math.random() * array.length)];
+    return randomElement;
+};
+
+
+let randomSuit = getRandomElement(suits);
+let randomValue = getRandomElement(values);
+
+
 window.onload = function() {
   //write your code here
- 
+
+  const mainContainer = document.getElementById("main-container"); 
+  const valueContainer = document.getElementById("value");
+  const leftSuit = document.getElementById("left");
+  const rightSuit = document.getElementById("right");
+
+  valueContainer.innerText = randomValue;
+  leftSuit.innerText = randomSuit;
+  rightSuit.innerText = randomSuit;
+  
+  if (randomSuit === "♦" || randomSuit === "♥") mainContainer.classList.add("red");
 };
